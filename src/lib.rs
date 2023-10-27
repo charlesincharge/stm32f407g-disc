af5a3a679d5370c4028536730f89bb5781c5c2e0
Charles Guan <3221512+charlesincharge@users.noreply.github.com>
2023-10-26 21:21:57 -0700

    stm32 alias is deprecated, and we should use pac instead

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@
 
 pub use stm32f4xx_hal as hal;
 
-pub use crate::hal::stm32::interrupt::*;
-pub use crate::hal::stm32::Interrupt as interrupt;
-pub use crate::hal::stm32::Peripherals;
-pub use crate::hal::stm32::*;
+pub use crate::hal::pac::interrupt::*;
+pub use crate::hal::pac::Interrupt as interrupt;
+pub use crate::hal::pac::Peripherals;
+pub use crate::hal::pac::*;
 pub use crate::hal::*;
 pub use cortex_m::*;
 pub use cortex_m_rt::*;
