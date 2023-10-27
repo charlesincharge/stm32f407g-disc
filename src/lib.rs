eecab622f319cf5396d679816783e8330c1a063c
Charles Guan <3221512+charlesincharge@users.noreply.github.com>
2023-10-26 21:22:13 -0700

    Remove unnecessary re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,8 @@
 pub use stm32f4xx_hal as hal;
 
 pub use crate::hal::pac::interrupt::*;
-pub use crate::hal::pac::Interrupt as interrupt;
+pub use crate::hal::pac::Interrupt;
 pub use crate::hal::pac::Peripherals;
-pub use crate::hal::pac::*;
-pub use crate::hal::*;
-pub use cortex_m::*;
-pub use cortex_m_rt::*;
 
 pub mod accelerometer;
 pub mod led;
